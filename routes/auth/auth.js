46178ff7f6f1b2e4e9538f08dfc2e2a6e6266668
Dominic Torruellas <dom@domtorr.com>
2019-08-31 15:56:32 -0700

    POST - /api/auth/register - Create a new user

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,4 +1,20 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const { getUserBy, addUser } = require('../../data/models/usersModel');
+
+router.post('/register', async (req, res) => {
+  try {
+    const userData = {
+      ...req.body,
+      password: await bcrypt.hashSync(req.body.password, 10),
+    };
+    res.status(201).json(await addUser(userData));
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Unable to create new user' });
+  }
+});
 
 router.get('/', (req, res) => {
   res.status(200).json({ message: 'Auth Router working' });
